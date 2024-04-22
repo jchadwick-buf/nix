440955d8601e86de1e1474bcb71a9aa59e59104d
John Chadwick <jchadwick@buf.build>
2024-04-22 10:26:24 -0400

    Updates, vim fixes, etc.

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -4,11 +4,21 @@
     ./vim
   ];
 
-  config = {
+  config =
+  let
+    # macOS devs REEEEEEEEEEEEEE
+    ggrep = pkgs.runCommand "ggrep" {} ''
+      mkdir -p $out/bin
+      ln -s ${pkgs.gnugrep}/bin/grep $out/bin/ggrep
+    '';
+  in {
     programs.nix-index.enable = true;
     environment.systemPackages = with pkgs; [
       # Utilities
+      comma
       git
+      ggrep
+      gnugrep
       jq
       yq
       ripgrep
@@ -24,6 +34,7 @@
       # Go toolchain
       unstable.go
       unstable.gopls
+      unstable.buf
     ];
 
     environment.variables = {
