02a2adb6984db6150303f449c7cc16d756bf9aef
John Chadwick <jchadwick@buf.build>
2024-06-03 13:29:01 -0400

    programs: cleanup, formatting, add gitu

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,45 +1,49 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   imports = [
     ./kitty
     ./vim
   ];
 
   config =
-  let
-    # macOS devs REEEEEEEEEEEEEE
-    ggrep = pkgs.runCommand "ggrep" {} ''
-      mkdir -p $out/bin
-      ln -s ${pkgs.gnugrep}/bin/grep $out/bin/ggrep
-    '';
-  in {
-    programs.nix-index.enable = true;
-    environment.systemPackages = with pkgs; [
-      # Utilities
-      comma
-      git
-      ggrep
-      gnugrep
-      jq
-      yq
-      ripgrep
+    let
+      # macOS devs REEEEEEEEEEEEEE
+      ggrep = pkgs.runCommand "ggrep" { } ''
+        mkdir -p $out/bin
+        ln -s ${pkgs.gnugrep}/bin/grep $out/bin/ggrep
+      '';
+    in
+    {
+      programs.nix-index.enable = true;
+      environment.systemPackages = with pkgs; [
+        # Utilities
+        comma
+        git
+        unstable.gitu
+        ggrep
+        gnugrep
+        jq
+        yq
+        ripgrep
+        nixfmt-rfc-style
 
-      # Kubernetes
-      k9s
+        # Kubernetes
+        k9s
 
-      # Cloud
-      awscli2
-      (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
-      google-cloud-sql-proxy
+        # Cloud
+        awscli2
+        (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+        google-cloud-sql-proxy
 
-      # Go toolchain
-      unstable.go
-      unstable.gopls
-      unstable.buf
-    ];
+        # Go toolchain
+        unstable.go
+        unstable.gopls
+        unstable.buf
+      ];
 
-    environment.variables = {
-      EDITOR = "vim";
-      GOPATH = "$HOME/Go";
+      environment.variables = {
+        EDITOR = "vim";
+        GOPATH = "$HOME/Go";
+      };
     };
-  };
 }
