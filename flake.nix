bd2e8452c8f4cdf03674ca61508728184a243fd5
John Chadwick <jchadwick@buf.build>
2024-06-03 13:26:41 -0400

    Update Nix flake dependencies

--- flake.nix
+++ flake.nix
@@ -2,33 +2,42 @@
   description = "My Nix configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
     nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim/nixos-23.11";
+    nixvim.url = "github:nix-community/nixvim"; # TODO: switch to 24.05 branch when available
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, nixpkgsUnstable, home-manager, nix-darwin, sops-nix, nixvim }: {
-    darwinConfigurations = {
-      "Johns-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        modules = [
-          home-manager.darwinModules.home-manager
-          nixvim.nixDarwinModules.nixvim
-          ./modules/machines/mbp
-          ./modules/base
-          ./modules/desktop
-          ./modules/users
-          ./modules/programs
-          ./modules/programs/homebrew
-        ];
-        specialArgs = { inherit inputs; };
+  outputs =
+    inputs@{
+      home-manager,
+      nix-darwin,
+      nixvim,
+      ...
+    }:
+    {
+      darwinConfigurations = {
+        "Johns-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            home-manager.darwinModules.home-manager
+            nixvim.nixDarwinModules.nixvim
+            ./modules/machines/mbp
+            ./modules/base
+            ./modules/desktop
+            ./modules/users
+            ./modules/programs
+            ./modules/programs/homebrew
+          ];
+          specialArgs = {
+            inherit inputs;
+          };
+        };
       };
     };
-  };
 }
